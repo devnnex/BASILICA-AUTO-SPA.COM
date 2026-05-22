1e33a1c908d3c63a75536c64221f2d0d2507a819
devnnex <tecnometaversesolutions@gmail.com>
2026-05-22 18:44:35 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwAyjc68mG9KJ-Fv-56dBxN-RI076wkt3TLDhCNpXthXQrEL5LfJQZgv_hzJQhywQJq/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbz_fTfwrLlFaC8i76q-D5jzIh54tR-asUwP31TPrQ9SXciIpJ11CcDOdLfCu8Fl6ocM/exec";
