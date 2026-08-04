b4c6cfcc345e6f9643e68ca7826201015cd36602
devnnex <devnnex@users.noreply.github.com>
2026-08-04 13:43:11 -0500

    Update Apps Script endpoint

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzW_SDzul7p8lmprVnN3fznfODBvI0kX3MQzEqTwG-rYPAggayFlYuzzOrJK9BrHi3pKg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyOIxtwJUmIumfs9lqYBZ8NfuKPoZ77BEmc755JV8LR7mXk6F9aEOcJ-Bc4kH83knblYg/exec";
